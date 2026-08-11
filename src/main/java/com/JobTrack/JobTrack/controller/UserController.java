c959a27afe59804ae54aac227be771220bbdcd4e
Ianischnf <81810701+Ianischnf@users.noreply.github.com>
2026-08-11 18:53:35 +0200

    Création du controller pour récupérer un utilisateur connecté

--- src/main/java/com/JobTrack/JobTrack/controller/UserController.java
+++ src/main/java/com/JobTrack/JobTrack/controller/UserController.java
@@ -24,6 +24,11 @@ public class UserController {
         return this.userService.updateUser(id, userRequestDTO);
     }
 
+    @PutMapping("/me")
+    public UserResponseDTO fetchCurrentUser(){
+        return this.userService.fetchCurrentUser();
+    }
+
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable("id") Long id) {
         this.userService.deleteUser(id);
